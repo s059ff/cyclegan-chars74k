a6fb7fca1ea77e9163bcee5aa07290808973dc41
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-07 11:21:37 +0000

    Adding tarfile member sanitization to extractall()

--- train.py
+++ train.py
@@ -39,7 +39,26 @@ def main():
         with open('dataset/EnglishHnd.tgz', 'wb') as stream:
             stream.write(response.read())
         with tarfile.open('dataset/EnglishHnd.tgz') as stream:
-            stream.extractall('dataset/')
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(stream, "dataset/")
         train = []
         for path in glob.iglob('dataset/English/Hnd/**/*.png', recursive=True):
             image = Image.open(path)
@@ -55,7 +74,26 @@ def main():
         with open('dataset/EnglishFnt.tgz', 'wb') as stream:
             stream.write(response.read())
         with tarfile.open('dataset/EnglishFnt.tgz') as stream:
-            stream.extractall('dataset/')
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(stream, "dataset/")
         train = []
         for path in glob.iglob('dataset/English/Fnt/**/*.png', recursive=True):
             image = Image.open(path)
